73d577c0bcbdb458b88c95bb2c705a25ac74237c
mjsb05 <maurijsb25@gmail.com>
2025-05-04 22:41:45 -0600

    Enviar imágenes y texto

--- lib/config/gemini/gemini_impl.dart
+++ lib/config/gemini/gemini_impl.dart
@@ -2,27 +2,56 @@ import 'dart:convert';
 
 import 'package:dio/dio.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:image_picker/image_picker.dart';
 
 class GeminiImpl {
   final Dio _http = Dio(BaseOptions(baseUrl: dotenv.env['ENDPOINT_API'] ?? ''));
 
   Future<String> getResponse(String prompt) async {
     try {
-      final body = jsonEncode({"prompt": prompt});
-      final response = await _http.post("/basic-prompt", data: body);
+      final body = jsonEncode({'prompt': prompt});
+      final response = await _http.post('/basic-prompt', data: body);
       return response.data;
     } catch (e) {
       print(e);
-      throw Exception("Error al obtener respuesta de Gemini");
+      throw Exception("Can't get Gemini response");
     }
   }
 
-  //Stream
-  Stream<String> getResponseStream(String prompt) async* {
-    //Todo: Tener presente que enviaremos imágenes
-    // Multipart
+  // Stream
+  Stream<String> getResponseStream(
+    String prompt, {
+    List<XFile> files = const [],
+  }) async* {
+    //! Multipart
+    final formData = FormData();
+    formData.fields.add(MapEntry('prompt', prompt));
 
-    final body = jsonEncode({"prompt": prompt});
-    final response = await _http.post("/basic-prompt-stream", data: body);
+    if (files.isNotEmpty) {
+      for (final file in files) {
+        formData.files.add(
+          MapEntry(
+            'files',
+            await MultipartFile.fromFile(file.path, filename: file.name),
+          ),
+        );
+      }
+    }
+
+    // final body = jsonEncode({'prompt': prompt});
+    final response = await _http.post(
+      '/basic-prompt-stream',
+      data: formData,
+      options: Options(responseType: ResponseType.stream),
+    );
+
+    final stream = response.data.stream as Stream<List<int>>;
+    String buffer = '';
+
+    await for (final chunk in stream) {
+      final chunkString = utf8.decode(chunk, allowMalformed: true);
+      buffer += chunkString;
+      yield buffer;
+    }
   }
 }
